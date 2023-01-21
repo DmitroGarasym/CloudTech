cef7d11596684768610ba02ab290e2a0de7116e5
Дмитро Гарасим <94192861+DmitroGarasym@users.noreply.github.com>
2023-01-21 09:07:13 +0200

    Update provision.sh

--- Lab1/provision.sh
+++ Lab1/provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-aws s3api create-bucket --bucket cloudlab1 --region us-east-1                              
+aws s3api create-bucket --bucket dmytrolab1 --region us-east-1                              
 
-aws s3api put-bucket-policy --bucket cloudlab1 --policy file://policy_s3.json         
+aws s3api put-bucket-policy --bucket dmytrolab1 --policy file://policy_s3.json         
 
-aws s3 sync ./ s3://cloudlab1/                                                             
+aws s3 sync ./ s3://dmytrolab1/                                                             
 
-aws s3 website s3://cloudlab1/ --index-document index.html --error-document error.html
+aws s3 website s3://dmytrolab1/ --index-document index.html --error-document error.html
 
-#website url: http://cloudlab1.s3-website-us-east-1.amazonaws.com/
+#website url: http://dmytrolab1.s3-website-us-east-1.amazonaws.com/
